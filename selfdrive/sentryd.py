a77b1cf7ed5aa88242cd1ef71671aa25bbac7fc8
royjr <royjr96@gmail.com>
2023-10-12 22:37:51 -0400

    base

--- selfdrive/sentryd.py
+++ selfdrive/sentryd.py
@@ -1,24 +1,46 @@
 #!/usr/bin/env python3
 import numpy as np
-
 from cereal import messaging
+
+THRESHOLD = 0.5  # Adjust as necessary based on sensitivity requirements
 
 
 class SentryMode:
   def __init__(self):
     self.sm = messaging.SubMaster(['accelerometer'], poll=['accelerometer'])
-
     self.prev_accel = np.zeros(3)
     self.initialized = False
 
+  def get_movement_type(self, current, previous):
+    diff = np.abs(current - previous)
+    ax_mapping = {0: "X-axis", 1: "Y-axis", 2: "Z-axis"}
+    dominant_axis = np.argmax(diff)
+    return ax_mapping[dominant_axis]
 
   def update(self):
     for sensor in self.sm['accelerometer']:
-      # do stuff here
+      accel_data = sensor['acceleration']['v']
+      curr_accel = np.array(accel_data)
 
+      if not self.initialized:
+        self.prev_accel = curr_accel
+        self.initialized = True
+        continue
+
+      magnitude_prev = np.linalg.norm(self.prev_accel)
+      magnitude_curr = np.linalg.norm(curr_accel)
+
+      delta = abs(magnitude_curr - magnitude_prev)
+
+      if delta > THRESHOLD:
+        movement_type = self.get_movement_type(curr_accel, self.prev_accel)
+        print("Movement Detected!")
+        print(f"Movement primarily in the direction of: {movement_type}")
+
+      self.prev_accel = curr_accel
 
   def start(self):
-    while 1:
+    while True:
       self.sm.update()
       self.update()
 
