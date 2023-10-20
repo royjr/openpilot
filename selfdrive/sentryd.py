d1ce52d91bc5aafdb6ccf6b623ab8a7502f9c095
royjr <royjr96@gmail.com>
2023-10-20 18:57:37 -0400

    Update sentryd.py

--- selfdrive/sentryd.py
+++ selfdrive/sentryd.py
@@ -57,7 +57,7 @@ class SentryMode:
       self.sentry_status = False
 
     # Trigger Status
-    if now_timestamp % 5 == 0:
+    if now_timestamp % 2 == 0:
       print("self.sentry_status: {}".format(self.sentry_status))
       self.prev_accel = curr_accel
 
